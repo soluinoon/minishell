22da0bc0fd5e6287ebb44a67aafce3e6d0200cca
eyeofsol <kingjihong@naver.com>
2023-01-05 13:20:08 +0900

    feat: HOME 환경변수 변경 시, ~ 환경변수도 업데이트  되는 기능 구현
    HOME 환경변수 변경 시, ~ 환경변수 역시 업데이트 되도록 만들었습니다.

--- srcs/builtin/ft_export_util.c
+++ srcs/builtin/ft_export_util.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export_util.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kko <kko@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jihonkim <jihonkim@42student.42seoul.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 20:31:59 by kko               #+#    #+#             */
-/*   Updated: 2022/12/12 20:32:26 by kko              ###   ########.fr       */
+/*   Updated: 2023/01/05 13:03:36 by jihonkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 void	check_export_util(t_token *token, int *flag, char **key, char **value)
 {
 	if (*flag == SUCCESS)
+	{
 		ft_putenv(token->info->env_list, *key, *value, 0);
+		if (ft_strncmp(*key, "HOME", 5) == 0)
+			ft_putenv(token->info->env_list, "~", *value, 0);
+	}
 	else if (*flag == 2)
+	{
 		ft_putenv(token->info->env_list, *key, *value, 1);
+	}
 	token->info->exit_num = 0;
 }
